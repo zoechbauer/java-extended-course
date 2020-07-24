fd99d8ef2d88d2d6bbea3de142e4e979a472b6f3
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-24 10:27:22 +0200

    Interface: Exercise 16

---
+++ Aufgabe16_Interface/src/Aufgabe16_Interface/Audi.java
@@ -0,0 +1,34 @@
+package Aufgabe16_Interface;
+
+public class Audi implements Driveable{
+	private String model;
+	private int horsePower;
+	private int positionX;
+	private int positionY;
+	
+	// ctor
+	public Audi(String model, int horsePower) {
+		this.model = model;
+		this.horsePower = horsePower;
+	}
+	
+	// methods
+		public void drive(int toPosX, int toPosY) {
+			System.out.printf("%s fährt von (%d/%d) nach (%d/%d)%n",
+					this.model, this.positionX, this.positionY, toPosX, toPosY);
+			 this.positionX += toPosX;
+			 this.positionY += toPosY;
+		}
+		
+		public void printPosition() {
+			System.out.printf("%s ist an der Position (%d/%d)%n", 
+					this.model, this.positionX, this.positionY);
+		}
+		
+		@Override
+		public String toString() {
+			return this.model + " mit " + this.horsePower + " PS";
+		}
+	
+	
+}
---
+++ Aufgabe16_Interface/src/Aufgabe16_Interface/Driveable.java
@@ -0,0 +1,7 @@
+package Aufgabe16_Interface;
+
+public interface Driveable {
+	void drive(int toPositionX, int toPositionY);
+	
+	void printPosition();
+}
