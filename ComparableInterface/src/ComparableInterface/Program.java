f9aec11717abf7f30931aadddd3b3c35db5184a6
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-25 13:43:25 +0200

    Comparable Interface for sorting objects

---
+++ ComparableInterface/src/ComparableInterface/Program.java
@@ -0,0 +1,28 @@
+package ComparableInterface;
+
+import java.util.Arrays;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// standard sort method 
+		String[] words= {"red", "green", "blue", "organge"};
+		Arrays.sort(words);
+		for(String word: words) {
+			System.out.println(word);
+		}
+		
+		// separate sort method
+		Student[] students = new Student[3];
+		students[0] = new Student("Max", "Mustermann", 22);
+		students[1] = new Student("Franz", "Müller", 33);
+		students[2] = new Student("Lisa", "Klein", 18);
+		
+		// sort students by age asc
+		Arrays.sort(students);
+		for(Student student : students) {
+			System.out.println(student);
+		}
+	}
+
+}
---
+++ ComparableInterface/src/ComparableInterface/Student.java
@@ -0,0 +1,30 @@
+package ComparableInterface;
+
+public class Student implements Comparable<Student>{
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String additionalInfos;
+	
+	// ctor
+	public Student(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	// methods
+	@Override
+	public int compareTo(Student o) {
+		if (!(o instanceof Student)) return 0;
+
+		Student other = (Student) o;
+		if (this.age < other.age) return -1;
+		if (this.age > other.age) return 1;
+		return 0;
+	};
+	
+	public String toString() {
+		return String.format("%s %s, Alter: %d", this.firstName, this.lastName, this.age);
+	}
+}
