8d61d5345034152b967ccd55754f83f954cfbbfc
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-26 13:22:11 +0200

    Exceptions: Excercise 21

---
+++ aufgabe21_exceptions/src/aufgabe21_exceptions/Heater.java
@@ -0,0 +1,23 @@
+package aufgabe21_exceptions;
+
+import java.util.Scanner;
+
+public class Heater {
+
+	private Byte level;
+	
+	public Heater() {};
+	
+	public boolean setLevel() throws Exception {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Bitte geben Sie die Heizungsstufe ein: ");
+		Byte inputByte = sc.nextByte();
+		if (inputByte < 0 || inputByte > 5) {
+			throw new WrongLevelException("Falsche Heizungsstufe (gültige Werte: 0-5)");
+		}
+		this.level = inputByte;
+		System.out.println("Neue Heizungsstufe: " + this.level);
+		return true;
+	}
+	
+}
---
+++ aufgabe21_exceptions/src/aufgabe21_exceptions/Program.java
@@ -0,0 +1,27 @@
+package aufgabe21_exceptions;
+
+import java.util.InputMismatchException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Heater heater = new Heater();
+		boolean levelSet = false;
+		
+		do {
+			try {
+				levelSet = heater.setLevel();
+			} catch (WrongLevelException e) {
+				System.out.printf("Ungültige Eingabe: %s%n", e.getMessage());
+			} catch (InputMismatchException e) {
+				System.out.println("Ungültige Eingabe: Sie dürfen nur Ziffern erfassen");
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}
+		} while(!levelSet);
+		
+		System.out.println("Ende");
+	}
+
+}
