3830336be38b05b2076979c3f244e443c50de14c
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-28 20:07:21 +0200

    Multithreading: Excercise 25

---
+++ aufgabe25_multithreading/src/aufgabe25_multithreading/Statistics.java
@@ -0,0 +1,27 @@
+package aufgabe25_multithreading;
+
+/**
+ * This class shows the usage of synchronized methods
+ * @author zoe
+ *
+ */
+public class Statistics {
+	
+	static int wordCounter = 0;
+	static int numberCounter = 0;
+	
+	public static synchronized void incrementWordCounter() {
+		wordCounter++;
+	}
+	
+	public static synchronized void incrementNumberCounter() {
+		 numberCounter++;
+	}
+	
+	public static void printStatistics() {
+		System.out.println("\nWörter und Zalen Statistik");
+		System.out.printf("Anzahl Wörter: %d, Anzahl Zahlen: %d, Gesamt: %d %n", 
+				wordCounter, numberCounter, wordCounter + numberCounter);
+	}
+
+}
---
+++ aufgabe25_multithreading/src/aufgabe25_multithreading/Utils.java
@@ -0,0 +1,26 @@
+package aufgabe25_multithreading;
+
+/**
+ * This Class is used by LetterThread and NumberThread
+ * @author zoe
+ *
+ */
+public class Utils {
+	
+	public static void randomDelay(){
+		int time = (int)  (Math.random() * 1000);
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// e.printStackTrace();
+			System.out.printf("%s schlief als die Nachricht *** %s *** kam.%n", Thread.currentThread().getName(), e.getMessage());
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	public static void printGeneratedString(String word) {
+		String threadNameString = Thread.currentThread().getName();
+		System.out.printf("%s : %s %n", threadNameString, word);
+	}
+	
+}
